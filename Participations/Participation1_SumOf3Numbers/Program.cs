ff64b4a2ecb2ae2ee1469da49e5a3837ecc4fe69
PaceSatterthwaite <PaceSatterthwaite@users.noreply.github.com>
2020-09-03 09:22:20 -0500

    Finished the Participation1 for Class

--- Participations/Participation1_SumOf3Numbers/Program.cs
+++ Participations/Participation1_SumOf3Numbers/Program.cs
@@ -6,7 +6,26 @@ namespace Participation1_SumOf3Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double total;
+            double totalWithExtras = 7.777;
+
+            Console.WriteLine("Please enter your first number >>");
+            double first = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter your second number >>");
+            double second = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter your third number >>");
+            double third = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Your Sum of Numbers Equals");
+            total = first + second + third;
+            String formattedNumberAsAString = total.ToString("F3");
+            Console.WriteLine(formattedNumberAsAString);
+
+            Console.WriteLine("The second value if you multiply the sum of the 3 numbers by a constant value of 7.777 would be >>");
+            total = total * totalWithExtras;
+            Console.WriteLine(total);
         }
     }
 }
